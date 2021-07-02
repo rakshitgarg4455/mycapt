eb8de30c53599228b736dae7e1ef2c8998e59cf2
rakshitgarg4455 <86824634+rakshitgarg4455@users.noreply.github.com>
2021-07-02 13:34:33 +0530

    Created swapping files.c

---
+++ swapping numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int a, b;
+	printf("a =");
+	scanf("%d", &a);
+	printf("b =");
+	scanf("%d, &b");
+	printf("before swapping the numbers, \n a = %d and b = %d\n", a, b);
+	printf("after swapping the numbers, \n a = %d and b = %d", b, a);
+	
+	return 0;
+}
